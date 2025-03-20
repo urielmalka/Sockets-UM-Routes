74cc1ae552082339177fc01bb21d7409cbe0a595
Uriel Malka <urielmalka1@gmail.com>
2025-03-20 15:03:11 +0200

    server update

--- server.cpp
+++ server.cpp
@@ -1,7 +1,44 @@
 #include "server.hpp"
 
 
-int main()
+Server::Server()
 {
-    return 0;
+    server_port = 8080;
+    max_connection = 128;
+    initServer();
+}
+
+Server::Server(int PORT)
+{
+    server_port = PORT;
+    max_connection = 128;
+    initServer();
+}
+
+Server::Server(int PORT, int max_conn)
+{
+    if(max_conn < 1 || PORT < 1) throw runtime_error("Values must be greater than zero.");
+
+    server_port = PORT;
+    max_connection = max_conn;
+    initServer();
+}
+
+Server::~Server(){}
+
+void Server::initServer()
+{
+    serverSocket =  socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(server_port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // binding socket.
+    bind(serverSocket, (struct sockaddr*)&serverAddress,sizeof(serverAddress));
+
+    // listening to the assigned socket
+    listen(serverSocket, max_connection);
+
+    cout << "Server is running on PORT: " << server_port << endl;
 }
--- server.hpp
+++ server.hpp
@@ -1,4 +1,36 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdexcept>
+
+using namespace std;
+
+class Server
+{
+    private:
+        
+        void initServer();
+        int serverSocket;
+        int server_port;
+        int max_connection;
+
+
+    public:
+        Server();
+        Server(int PORT);
+        Server(int PORT, int max_conn);
+        ~Server();
+
+        void addRoute();
+    
+};
+
+
+
+
 #endif
