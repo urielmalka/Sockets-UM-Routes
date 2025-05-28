cee0e17c88b0d8b060cfa8571d7e49934f4b2792
Uriel Malka <urielmalka1@gmail.com>
2025-05-28 12:42:22 +0300

    FIX SUCKUM.HPP

--- Sockum/include/Sockum.hpp
+++ Sockum/include/Sockum.hpp
@@ -1,20 +1,21 @@
-#ifndef SOCKUM_HPP
-#define SOCKUM_HPP
-
-/*
-
-  ███████  ███████  ██████ ██  ███ ██   ██ ███  ███
-  ██       ██   ██ ██      ██ ██   ██   ██ ████████
-  ███████  ██   ██ ██      ████    ██   ██ ██ ██ ██
-       ██  ██   ██ ██      ██  ██  ██   ██ ██    ██
-  ███████  ███████  ██████ ██   ██ ███████ ██    ██
-
-  @author Uriel Malka - urielmalka1@gmail.com
-  @version 1.0.0
-  @brief `sockum` is a C++ client-server communication framework using TCP sockets. It supports multiple simultaneous clients, custom route handling, and a modular design for extending server/client functionality.
+/**
+*
+*  ███████  ███████  ██████ ██  ███ ██   ██ ███  ███
+*  ██       ██   ██ ██      ██ ██   ██   ██ ████████
+*  ███████  ██   ██ ██      ████    ██   ██ ██ ██ ██
+*       ██  ██   ██ ██      ██  ██  ██   ██ ██    ██
+*  ███████  ███████  ██████ ██   ██ ███████ ██    ██
+*
+*  @author Uriel Malka - urielmalka1@gmail.com
+*  @version 1.0.0
+*  @brief `sockum` is a C++ client-server communication framework using TCP sockets. It supports multiple simultaneous clients, 
+            custom route handling, and a modular design for extending server/client functionality.
 */
 
 
+
+#ifndef SOCKUM_HPP
+#define SOCKUM_HPP
 
 #include "server.hpp"
 #include "client.hpp"
