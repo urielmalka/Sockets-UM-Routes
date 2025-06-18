c6e5c410ada4ea576013bfdbb9cd9639bf0f9535
Uriel Malka <urielmalka1@gmail.com>
2025-06-18 14:48:40 +0300

    Room

--- Sockum/src/room.cpp
+++ Sockum/src/room.cpp
@@ -2,9 +2,27 @@
 
 Room::Room(const std::string &room_name)
 {
+    this->room_name = room_name;
 }
 
-Room::~Room()
-{
+Room::~Room(){}
 
+bool Room::clientJoin(std::string client_id, int client_socket)
+{
+    if (clients.find(client_id) != clients.end()) {
+        return false; // Client already in the room
+    }
+    
+    clients[client_id] = client_socket;
+    return true;
+}
+    
+bool Room::clientLeave(std::string client_id)
+{
+    auto it = clients.find(client_id);
+    if (it != clients.end()) {
+        clients.erase(it);
+        return true;
+    }
+    return false;
 }
