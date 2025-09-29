cdb4af57d417594827748e42082a5f47cd489e68
Uriel Malka <urielmalka1@gmail.com>
2025-09-29 22:40:31 +0300

    @version 1.1.9

--- Sockum/include/Sockum.hpp
+++ Sockum/include/Sockum.hpp
@@ -7,7 +7,7 @@
 *  ███████  ███████  ██████ ██   ██ ███████ ██    ██
 *
 *  @author Uriel Malka - urielmalka1@gmail.com
-*  @version 1.1.8
+*  @version 1.1.9
 *  @brief `sockum` is a C++ client-server communication framework using TCP sockets. It supports multiple simultaneous clients, 
             custom route handling, and a modular design for extending server/client functionality.
 */
